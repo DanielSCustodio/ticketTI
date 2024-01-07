b6e786e75f7362e90b8b36c6e2a343b2d48d81d5
Daniel Custódio <play.daniel@hotmail.com>
2024-01-06 21:00:01 -0300

    feat: Login and logout method implemented.

--- routes/Auth.js
+++ routes/Auth.js
@@ -4,11 +4,9 @@ const AuthController = require('../controller/Auth');
 
 //Get
 router.get('/login', AuthController.login);
-router.get('/registro', AuthController.register);
 router.get('/logout', AuthController.logout);
 
 //Post
-router.post('/registro', AuthController.registerUser);
 router.post('/login', AuthController.loginUser);
 
 module.exports = router;
