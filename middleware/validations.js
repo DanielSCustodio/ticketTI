aa394d8b84c091091a19265b66e467c1f41d6aef
Daniel Custódio <play.daniel@hotmail.com>
2024-01-14 13:53:32 -0300

    feat: Validation of the equipment registration form.

--- middleware/validations.js
+++ middleware/validations.js
@@ -1,6 +1,9 @@
 const Departament = require('../models/Departament');
 const Institution = require('../models/Institution');
+const Person = require('../models/Person');
+const ReferenceType = require('../models/ReferenceType');
 
+//Institution, Departament, ReferenceType
 module.exports.checkNameInput = function (req, res, next) {
   const response = { name: req.body.name };
   const urlString = req.protocol + '://' + req.get('host') + req.originalUrl;
@@ -20,6 +23,7 @@ module.exports.checkNameInput = function (req, res, next) {
   next();
 };
 
+//Person
 module.exports.checkPerson = async function async(req, res, next) {
   const { name, role, institutionSelected, departamentSelected } = req.body;
   const departaments = await Departament.findAll({ raw: true });
@@ -28,7 +32,7 @@ module.exports.checkPerson = async function async(req, res, next) {
   if (name.length <= 3) {
     req.flash(
       'error-input-person',
-      'O campo nome deve conter pelo menos 4 caracteres.',
+      'O campo "nome" deve conter pelo menos 4 caracteres. ',
     );
     res.render('colaborador/create', { departaments, institutions });
     return;
@@ -37,22 +41,90 @@ module.exports.checkPerson = async function async(req, res, next) {
   if (role.length <= 3) {
     req.flash(
       'error-input-person',
-      'O campo função deve conter pelo menos 4 caracteres.',
+      'O campo "função" deve conter pelo menos 4 caracteres.',
     );
     res.render('colaborador/create', { departaments, institutions });
     return;
   }
 
   if (!institutionSelected) {
-    req.flash('error-input-person', 'O campo instituição deve ser preenchido.');
+    req.flash(
+      'error-input-person',
+      'O campo "instituição" deve ser preenchido. Clique na lupa para selecionar.',
+    );
     res.render('colaborador/create', { departaments, institutions });
     return;
   }
 
   if (!departamentSelected) {
-    req.flash('error-input-person', 'O campo setor deve ser preenchido.');
+    req.flash(
+      'error-input-person',
+      'O campo "setor" deve ser preenchido. Clique na lupa para selecionar.',
+    );
     res.render('colaborador/create', { departaments, institutions });
     return;
   }
+  next();
+};
+
+//Equipment
+module.exports.checkEquipment = async function async(req, res, next) {
+  const {
+    name,
+    personSelected,
+    reference,
+    referenceTypeSelected,
+    departamentSelected,
+  } = req.body;
+
+  const departaments = await Departament.findAll({ raw: true });
+  const people = await Person.findAll({ raw: true });
+  const referenceType = await ReferenceType.findAll({ raw: true });
+
+  if (name.length <= 3) {
+    req.flash(
+      'error-input-equipment',
+      'O campo "equipamento" deve conter pelo menos 4 caracteres. ',
+    );
+    res.render('equipamento/create', { departaments, people, referenceType });
+    return;
+  }
+
+  if (!personSelected) {
+    req.flash(
+      'error-input-equipment',
+      'O campo "colaborador" deve ser preenchido. Clique na lupa para selecionar.',
+    );
+    res.render('equipamento/create', { departaments, people, referenceType });
+    return;
+  }
+
+  if (!departamentSelected) {
+    req.flash(
+      'error-input-equipment',
+      'O campo "setor" deve ser preenchido. Clique na lupa para selecionar.',
+    );
+    res.render('equipamento/create', { departaments, people, referenceType });
+    return;
+  }
+
+  if (!referenceTypeSelected) {
+    req.flash(
+      'error-input-equipment',
+      'O campo "tipo de referência" deve ser preenchido. Clique na lupa para selecionar.',
+    );
+    res.render('equipamento/create', { departaments, people, referenceType });
+    return;
+  }
+
+  if (reference.length <= 3) {
+    req.flash(
+      'error-input-equipment',
+      'O campo "referência" deve conter pelo menos 4 caracteres. ',
+    );
+    res.render('equipamento/create', { departaments, people, referenceType });
+    return;
+  }
+
   next();
 };
--- routes/Equipment.js
+++ routes/Equipment.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const EquipmentController = require('../controller/Equipment');
+const checkEquipment = require('../middleware/validations').checkEquipment;
 
 router.get('/cadastro', EquipmentController.createEquipment);
-router.post('/add', EquipmentController.createEquipmentSave);
+router.post('/add', checkEquipment, EquipmentController.createEquipmentSave);
 router.get('/', EquipmentController.viewEquipments);
 
 module.exports = router;
