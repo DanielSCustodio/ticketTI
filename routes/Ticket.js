b478ec613fa6e5b6476c5c072f03628ab03d2cd4
Daniel Custódio <play.daniel@hotmail.com>
2024-01-15 13:51:42 -0300

    feat: Validation of the ticket registration form.

--- routes/Ticket.js
+++ routes/Ticket.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const TicketController = require('../controller/Ticket');
 const checkPrivilege = require('../middleware/validations').checkPrivilege;
+const checkTicket = require('../middleware/validations').checkTicket;
 
 router.get('/cadastro', checkPrivilege, TicketController.createTicket);
-router.post('/add', TicketController.createTicketSave);
+router.post('/add', checkTicket, TicketController.createTicketSave);
 router.get('/', TicketController.viewTickets);
 
 module.exports = router;
