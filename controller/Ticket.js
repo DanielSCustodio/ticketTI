3bdc2ddbba825b9a8021754b03334293b99aaa62
Daniel Custódio <play.daniel@hotmail.com>
2024-01-20 17:51:29 -0300

    feat: Method for deleting a ticket.

--- controller/Ticket.js
+++ controller/Ticket.js
@@ -38,66 +38,101 @@ module.exports = class TicketController {
       equipmentSelected,
     } = req.body;
 
-    const id = (await Ticket.findAll({ raw: true })).length + 1;
+    /*  const id = (await Ticket.findAll({ raw: true })).length + 1; */
 
-    const supportAgent = await Person.findOne({
-      raw: true,
-      where: { name: administratorIdSelected },
-    });
+    try {
+      const supportAgent = await Person.findOne({
+        raw: true,
+        where: { name: administratorIdSelected },
+      });
 
-    const departament = await Departament.findOne({
-      raw: true,
-      where: { name: departamentSelected },
-    });
+      const departament = await Departament.findOne({
+        raw: true,
+        where: { name: departamentSelected },
+      });
 
-    const requester = await Person.findOne({
-      raw: true,
-      where: { name: requesterSelected },
-    });
+      const requester = await Person.findOne({
+        raw: true,
+        where: { name: requesterSelected },
+      });
 
-    const equipment = await Equipment.findOne({
-      raw: true,
-      where: { name: equipmentSelected },
-    });
+      const equipment = await Equipment.findOne({
+        raw: true,
+        where: { name: equipmentSelected },
+      });
 
-    const ticket = {
-      title,
-      description,
-      solution,
-      date,
-      startTime,
-      endTime,
-      AdministratorId: supportAgent.id,
-      DepartamentId: departament.id,
-      PersonId: requester.id,
-      EquipmentId: equipment.id,
-    };
+      const ticket = {
+        title,
+        description,
+        solution,
+        date,
+        startTime,
+        endTime,
+        AdministratorId: supportAgent.id,
+        DepartamentId: departament.id,
+        PersonId: requester.id,
+        EquipmentId: equipment.id,
+      };
+      req.flash('create-ticket', `Ticket "${title}" criado com sucesso.`);
 
-    await Ticket.create(ticket);
+      await Ticket.create(ticket);
 
-    res.redirect(`/ticket/${id}`);
+      res.redirect('/ticket');
+    } catch (error) {
+      console.log('Aconteceu um erro ===>', error);
+    }
   }
 
-  // Controller
-  static async viewTickets(_req, res) {
-    let tickets = await Ticket.findAll({
-      include: [
-        { model: Departament },
-        { model: Person },
-        {
-          model: Administrator,
-          include: [{ model: Person, attributes: ['name'] }],
-        },
-        { model: Equipment },
-      ],
-    });
+  static async viewTickets(req, res) {
+    const id = req.session.userid;
 
-    tickets = tickets.map((result) => {
-      const plainResult = result.get({ plain: true });
-      // Renomeia o campo AdministratorId para AdministratorName
-      plainResult.AdministratorName = plainResult.Administrator?.Person?.name;
-      return plainResult;
-    });
-    res.render('ticket/all', { tickets });
+    try {
+      const user = await Administrator.findOne({
+        where: { id: id },
+      });
+      let privilege = user.privilege;
+
+      let tickets = await Ticket.findAll({
+        include: [
+          { model: Departament },
+          { model: Person },
+          {
+            model: Administrator,
+            include: [{ model: Person, attributes: ['name'] }],
+          },
+          { model: Equipment },
+        ],
+      });
+
+      tickets = tickets.map((result) => {
+        const plainResult = result.get({ plain: true });
+        // Renomeia o campo AdministratorId para AdministratorName
+        plainResult.AdministratorName = plainResult.Administrator?.Person?.name;
+        return plainResult;
+      });
+      res.render('ticket/all', { tickets, privilege });
+    } catch (error) {
+      console.log('Aconteceu um erro ===>', error);
+    }
+  }
+
+  static async removeTicket(req, res) {
+    const id = req.body.id;
+    try {
+      const ticket = await Ticket.findOne({
+        where: { id: id },
+        raw: true,
+      });
+      await Ticket.destroy({ where: { id: id } });
+      req.flash(
+        'delete-ticket',
+        `Ticket "${ticket.title}" excluído com sucesso.`,
+      );
+      req.session.save(() => {
+        res.redirect('/ticket');
+      });
+    } catch (error) {
+      console.log('Aconteceu um erro ===>', error);
+    }
   }
 };
--- routes/Ticket.js
+++ routes/Ticket.js
@@ -4,8 +4,12 @@ const TicketController = require('../controller/Ticket');
 const checkPrivilege = require('../middleware/validations').checkPrivilege;
 const checkTicket = require('../middleware/validations').checkTicket;
 
+//get
 router.get('/cadastro', checkPrivilege, TicketController.createTicket);
-router.post('/add', checkTicket, TicketController.createTicketSave);
 router.get('/', TicketController.viewTickets);
+
+//post
+router.post('/remove', checkPrivilege, TicketController.removeTicket);
+router.post('/add', checkTicket, TicketController.createTicketSave);
 
 module.exports = router;
