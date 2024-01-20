fed1db7af0659b7bf6e1b99cf087efb3d5d0cee7
Daniel Custódio <play.daniel@hotmail.com>
2024-01-20 17:10:05 -0300

    feat: Method for deleting an equipment/application.

--- routes/Equipment.js
+++ routes/Equipment.js
@@ -4,8 +4,12 @@ const EquipmentController = require('../controller/Equipment');
 const checkEquipment = require('../middleware/validations').checkEquipment;
 const checkPrivilege = require('../middleware/validations').checkPrivilege;
 
+//get
 router.get('/cadastro', checkPrivilege, EquipmentController.createEquipment);
-router.post('/add', checkEquipment, EquipmentController.createEquipmentSave);
 router.get('/', EquipmentController.viewEquipments);
+
+//post
+router.post('/remove', checkPrivilege, EquipmentController.removeEquipment);
+router.post('/add', checkEquipment, EquipmentController.createEquipmentSave);
 
 module.exports = router;
