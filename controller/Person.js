52ddd16c2940d7c227a160fc8766380aae6b30dd
Daniel Custódio <play.daniel@hotmail.com>
2024-01-20 16:15:55 -0300

    feat: Method for deleting a person.

--- controller/Person.js
+++ controller/Person.js
@@ -1,6 +1,7 @@
 const Departament = require('../models/Departament');
 const Institution = require('../models/Institution');
 const Person = require('../models/Person');
+const Administrator = require('../models/Administrator');
 
 module.exports = class PersonController {
   static async createPerson(_req, res) {
@@ -13,32 +14,68 @@ module.exports = class PersonController {
     const { name, role, institutionSelected, departamentSelected } = req.body;
     /*     const id = (await Person.findAll({ raw: true })).length + 1; */
 
-    const departament = await Departament.findOne({
-      raw: true,
-      where: { name: departamentSelected },
-    });
-
-    const institution = await Institution.findOne({
-      raw: true,
-      where: { name: institutionSelected },
-    });
-    const person = {
-      name,
-      role,
-      InstitutionId: institution.id,
-      DepartamentId: departament.id,
-    };
-
-    await Person.create(person);
-
-    res.redirect(`/colaborador`);
+    try {
+      const departament = await Departament.findOne({
+        raw: true,
+        where: { name: departamentSelected },
+      });
+      const institution = await Institution.findOne({
+        raw: true,
+        where: { name: institutionSelected },
+      });
+      const person = {
+        name,
+        role,
+        InstitutionId: institution.id,
+        DepartamentId: departament.id,
+      };
+      req.flash(
+        'create-person',
+        `Colaborador "${name}" cadastrado com sucesso.`,
+      );
+      await Person.create(person);
+      res.redirect(`/colaborador`);
+    } catch (error) {
+      console.log('Aconteceu um erro ===>', error);
+    }
   }
 
-  static async viewPeople(_req, res) {
-    let people = await Person.findAll({
-      include: [{ model: Departament }, { model: Institution }],
-    });
-    people = people.map((result) => result.get({ plain: true }));
-    res.render('colaborador/all', { people });
+  static async viewPeople(req, res) {
+    const id = req.session.userid;
+
+    try {
+      const user = await Administrator.findOne({
+        where: { id: id },
+      });
+      let privilege = user.privilege;
+
+      let people = await Person.findAll({
+        include: [{ model: Departament }, { model: Institution }],
+      });
+      people = people.map((result) => result.get({ plain: true }));
+      res.render('colaborador/all', { people, privilege });
+    } catch (error) {
+      console.log('Aconteceu um erro ===>', error);
+    }
+  }
+
+  static async removePerson(req, res) {
+    const id = req.body.id;
+    try {
+      const person = await Person.findOne({
+        where: { id: id },
+        raw: true,
+      });
+      await Person.destroy({ where: { id: id } });
+      req.flash(
+        'delete-person',
+        `Colaborador "${person.name}" excluído com sucesso.`,
+      );
+      req.session.save(() => {
+        res.redirect('/colaborador');
+      });
+    } catch (error) {
+      console.log('Aconteceu um erro ===>', error);
+    }
   }
 };
--- routes/Person.js
+++ routes/Person.js
@@ -4,8 +4,13 @@ const PersonController = require('../controller/Person');
 const checkPerson = require('../middleware/validations').checkPerson;
 const checkPrivilege = require('../middleware/validations').checkPrivilege;
 
+//get
 router.get('/cadastro', checkPrivilege, PersonController.createPerson);
 router.post('/add', checkPerson, PersonController.createPersonSave);
+
+//post
+router.post('/remove', checkPrivilege, PersonController.removePerson);
+
 router.get('/', PersonController.viewPeople);
 
 module.exports = router;
